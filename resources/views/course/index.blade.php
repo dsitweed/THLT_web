117f9b8600348e9c5ca02d7d5cccdafa109605f7
khang <khang.td194591@sis.hust.edu.vn>
2022-12-28 07:30:59 +0700

    update

--- resources/views/course/index.blade.php
+++ resources/views/course/index.blade.php
@@ -3,32 +3,35 @@
 {{-- Nhận 1 biến $listCourse array kiểu Model Course --}}
 
 @php
-    if (!isset($listCourse)) $listCourse = [];
+    if (!isset($listCourse)) {
+        $listCourse = [];
+    }
 @endphp
 
 @section('content')
-    <div class="mx-2">
-        <table class="table-fixed w-full border border-spacing-2 border-collapse border-slate-400">
-            <thead>
-                <?php $thClass = "border p-2 border-slate-400" ?>
-                <tr>
-                    <th class="{{$thClass}}">ID khóa học</th>
-                    <th class="{{$thClass}}">Tên khóa học</th>
-                    <th class="{{$thClass}}">Miêu tả khóa học</th>
-                    {{-- <th class="{{$thClass}}">Số học sinh đang tham gia</th> --}}
-                </tr>
-            </thead>
-            <tbody>
-                <?php $tdClass = "text-center border border-slate-400 p-2" ?>
-                @foreach ($listCourse as $item)
-                    <tr>
-                        <td class="{{$tdClass}}">{{$item->id}}</td>
-                        <td class="{{$tdClass}}">{{$item->name}}</td>
-                        <td class="{{$tdClass}}">{{$item->description}}</td>
-                        <td class="{{$tdClass}}">{{$item->description}}</td>
+    <div class="p-3 flex-1 bg-slate-100">
+        <div class="my-6 bg-white mx-4">
+            <table class="w-full">
+                <thead>
+                    <?php $thClass = 'bg-yellow-400 text-white px-4 py-3'; ?>
+                    <tr class="text-md font-semibold">
+                        <th class="{{ $thClass }}">ID khóa học</th>
+                        <th class="{{ $thClass }}">Tên khóa học</th>
+                        <th class="{{ $thClass }}">Miêu tả khóa học</th>
+                        {{-- <th class="{{$thClass}}">Số học sinh đang tham gia</th> --}}
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    <?php $tdClass = 'text-center px-4 py-3'; ?>
+                    @foreach ($listCourse as $item)
+                        <tr class="hover:bg-yellow-100">
+                            <td class="{{ $tdClass }}">{{ $item->id }}</td>
+                            <td class="{{ $tdClass }}">{{ $item->name }}</td>
+                            <td class="{{ $tdClass }}">{{ $item->description }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
     </div>
 @endsection
